014e2cef43c448b01e9e68efc307b4ef9fe9bf1e
mariuszlas <mariusz.las@outlook.com>
2021-10-09 21:58:23 +0100

    Added a test for navbar and footer

---
+++ client/src/layout/Footer/Footer.test.js
@@ -0,0 +1,12 @@
+import { screen } from '@testing-library/react';
+import Footer from '.';
+
+describe('Footer', () => {
+
+    it('renders footer', () => {
+        renderWithReduxAndRouter(<Footer />);
+        const footer = screen.getByRole('contentinfo');
+        expect(footer.textContent)
+            .toBe('Created by Cerise Abel-Thompson, Owen Jenkins, Simon Allan & Mariusz Las')
+    });
+});
