22489fe3fd2257da9ea29d2b800c74aea166d53f
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-10-09 16:41:14 +0100

    restructured parametrize

--- api/users/tests.py
+++ api/users/tests.py
@@ -1,24 +1,26 @@
-import pytest
 from django.contrib.auth import get_user_model
 from .models import Company
 from django.test import TestCase
 
+import pytest
 pytestmark = pytest.mark.django_db
 
+"""
+Tests the creation of the CustomUser model.
+"""
 
-class UsersManagersTests(TestCase):
-
+class TestCustomUserManager(TestCase):
 
     @pytest.mark.django_db
     def test_can_create_user(self):
 
+        # Company is currently stub (@OGWJ 09-10-21)
         test_company = Company.objects.create()
         User = get_user_model()
-
-        # Company is currently stub (@OGWJ 09-10-21)
         user = User.objects.create_user(email='test@user.com', password='foo',
                                         first_name='first', surname='last',
                                         company=test_company)
+
         self.assertEqual(user.email, 'test@user.com')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
@@ -28,25 +30,25 @@ class UsersManagersTests(TestCase):
             self.assertIsNone(user.username)
         except AttributeError:
             pass
-    
+
+
     @pytest.mark.django_db
     def test_cannot_create_user(self):
 
         """
         Assert cannot create user with any missing field.
-        NOTE: Nested function definition required to allow both parametrize and django-db access decorators. 
         """
 
         test_company = Company.objects.create()
         User = get_user_model()
 
-        failing_user_creation_test_cases = [(None, 'baz', 'first', 'last', test_company),
-                                            ('foo@bar.com', None, 'first', 'last', test_company),
-                                            ('foo@bar.com', 'baz', None, 'last', test_company),
-                                            ('foo@bar.com', 'baz', 'first', None, test_company),
-                                            ('foo@bar.com', 'baz', 'first', 'last', None)]
-
-        @pytest.mark.parametrize("email, password, first_name, surname, company", failing_user_creation_test_cases)
-        def test_failing_cases(self, email, password, first_name, surname, company):
-            with self.assertRaises(TypeError):
-                User.objects.create_user(email=email, password=password, first_name=first_name, surname=surname, company=company)
+        # NOTE: Cannot parametrize using pytest decorator due to django_db error.
+        failing_params = [#(None, 'baz', 'first', 'last', test_company),
+                          ('foo@bar.com', None, 'first', 'last', test_company),
+                          ('foo@bar.com', 'baz', None, 'last', test_company),
+                          ('foo@bar.com', 'baz', 'first', None, test_company),
+                          ('foo@bar.com', 'baz', 'first', 'last', None)]
+        
+        with self.assertRaises(TypeError):
+            for e, p, f, s, c in failing_params:
+                User.objects.create_user(email=e, password=p, first_name=f, surname=s, company=c)
