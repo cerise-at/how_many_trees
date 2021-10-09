e6e259907a73ee5f07830b081b8cbd1d00414f30
mariuszlas <mariusz.las@outlook.com>
2021-10-09 21:56:16 +0100

    Configured setupTests file

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,1 +1,43 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
 import '@testing-library/jest-dom';
+import "@testing-library/jest-dom/extend-expect";
+
+import userEvent from "@testing-library/user-event";
+import { render } from '@testing-library/react';
+import { createStore, applyMiddleware } from 'redux';
+import reducer from './redux/reducer'
+
+const TestProviders = ({ initState }, history) => {
+    initState ||= {};
+
+    const testReducer = () => reducer(initState, { type: '@@INIT' });
+    const testStore = createStore(testReducer);
+
+    return ({ children }) => (
+        <Provider store={testStore}>
+            <BrowserRouter>
+                { children }
+            </BrowserRouter>
+        </Provider>
+    )
+}
+
+const renderWithReduxAndRouter = (uiElement, options={}) => {
+    let TestWrapper = TestProviders(options)
+    render(uiElement, { wrapper: TestWrapper, ...options })
+}
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn(),
+};
+
+global.renderWithReduxAndRouter = renderWithReduxAndRouter
+global.React = React;
+global.render = render;
+global.userEvent = userEvent;
+global.localStorage = localStorageMock;
