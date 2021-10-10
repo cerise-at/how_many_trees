1d4337e3f3c482ac3a8b9708b9d8ebf93681cf07
mariuszlas <mariusz.las@outlook.com>
2021-10-10 19:33:00 +0100

    Added routes list component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,5 @@
 export { default as AuthRoute } from './AuthRoute';
 export { default as LoginForm } from './LoginForm';
 export { default as PrivateRoute } from './PrivateRoute';
+export { default as RoutesList } from './RoutesList';
 export { default as RegisterForm } from './RegisterForm';
--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { NavBar } from '../../layout';
+import { RoutesList } from '../../components';
 
 function Dashboard() {
 
     return (
         <>
             <NavBar />
+            <div className="d-flex justify-content-center">
+                <RoutesList />
+            </div>
         </>
     );
 }
