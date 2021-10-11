d5ed78bc1535df500cebe5fc01ed863d83989d37
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-10-11 17:48:05 +0100

    added token check to dashboard endpoint

--- api/users/views.py
+++ api/users/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from .models import CustomUser
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 import json
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def dashboard(request, user_email):
     # print('dashboard received user_email: ', user_email)
     user = get_object_or_404(CustomUser, pk=user_email)
