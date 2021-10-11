be68a07d14f2e1dfe1e385e4140ee9c6a49bdd34
Simon Allan <stallan464@googlemail.com>
2021-10-11 11:01:47 +0100

    stabbing wildly at django api requests

--- api/routes/models.py
+++ api/routes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+     def __init__(self, data):
+        self.co2Emissions = data["co2Emissions"]
+        self.revenueWeight = data["revenueWeight"]
--- api/routes/views.py
+++ api/routes/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+# def data_request(registrationNumber, co2Emissions, revenueWeight):
+#     url = f"https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles?registrationNumber={registrationNumber}&co2Emissions={co2Emissions}&revenueWeight={revenueWeight}&key=4374ac90-b6ff-4b8c-a86e-809564970414"
+#     resp = requests.get(url)
+#     return resp.json()
+
+import requests
+
+url = "https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles"
+
+payload = "{\n\t\"registrationNumber\": \"AA19AAA\"\n}"
+headers = {
+  'x-api-key': '4374ac90-b6ff-4b8c-a86e-809564970414',
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
