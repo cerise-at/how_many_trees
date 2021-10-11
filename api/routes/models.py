aea9aa4a69aaeee7abbd30e17f67e02328ee6d32
“cerise-at <cerise.athompson>
2021-10-11 15:49:06 +0100

    gets route coordinates from mapbox

--- api/routes/models.py
+++ api/routes/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Car(models.Model):
        
-       co2_emissions = models.IntegerField(max_length=50)
-       revenue_weight = models.IntegerField(max_length=50)
+       co2_emissions = models.IntegerField
+       revenue_weight = models.IntegerField
        reg_plate = models.CharField(max_length=50)
 
    #   def __init__(self, data):
--- api/routes/views.py
+++ api/routes/views.py
@@ -27,16 +27,16 @@ def get_directions_info(request):
       if request.method == 'GET':
             startpoint = 'ba46bn'
             endpoint = 'wolverhampton'
-            geo_points = get_lat_long(startpoint, endpoint)
-            print(geo_points)
+            coords = get_lat_long(startpoint, endpoint)
+            print(coords)
 
             # headers= { 'alternatives: true', 'origin: {request.orig}', 'destination: {request.dest}'}
-            url = f'https://api.mapbox.com/directions/v5/driving{geo_points}'
+            url = f'https://api.mapbox.com/directions/v5/mapbox/driving/{coords}?geometries=geojson&access_token=pk.eyJ1IjoiY2VyaXNlLWF0IiwiYSI6ImNrdW1ycG54cDBkZ3MzMW9hYjY4dnAwNXMifQ.gsFC-xmHmsp-EneBn8yrQQ'
       
-            response = requests.request("POST", url)
+            response = requests.request("GET", url)
             
             data = response.json()
-            print(data)
+           
             routes = data['routes']
             if len(routes) <1:
                   one = data["geocoded_waypoints"][0]["place_id"]
@@ -55,13 +55,14 @@ def get_lat_long(startpoint, endpoint):
       url_start_lat_long = f'https://api.mapbox.com/geocoding/v5/mapbox.places/{startpoint}.json?country=gb&access_token=pk.eyJ1IjoiY2VyaXNlLWF0IiwiYSI6ImNrdW1wMWhhaTAxMjAydWp0YnExa2lsanAifQ.11WeE94rbtUkNefoue_dSQ'
       start_response = requests.request('GET', url_start_lat_long)
       start_data = start_response.json()
-      coords = start_data['features'][0]['geometry']['coordinates']
+      start = start_data['features'][0]['geometry']['coordinates']
+      
       
       url_end_lat_long = f'https://api.mapbox.com/geocoding/v5/mapbox.places/{endpoint}.json?country=gb&access_token=pk.eyJ1IjoiY2VyaXNlLWF0IiwiYSI6ImNrdW1wMWhhaTAxMjAydWp0YnExa2lsanAifQ.11WeE94rbtUkNefoue_dSQ'
       end_response = requests.request('GET', url_end_lat_long)
       end_data = end_response.json()
-      coords.extend(end_data['features'][0]['geometry']['coordinates'])
-
+      end = (end_data['features'][0]['geometry']['coordinates'])
+      coords = f"{start[0]},{start[1]};{end[0]},{end[1]}"
       return coords
 
 
