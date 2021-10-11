12bdd1d556c86d7418679a9c2fd402def6a473b3
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-10-11 12:03:16 +0100

    _

--- api/users/views.py
+++ api/users/views.py
@@ -9,4 +9,4 @@ def dashboard(request, user_email):
     user = get_object_or_404(CustomUser, pk=user_email)
     print(user.get_dashboard())
     # return Response(json.dumps(user.get_dashboard()), content_type='application/json')
-    return Response(json.dumps({"status": 200}))
+    return Response(json.dumps({"status": 200}), content_type='application/json')
