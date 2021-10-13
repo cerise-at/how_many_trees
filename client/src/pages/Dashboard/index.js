16ceae2115423c76b517f80cae56032aae70846f
mariuszlas <mariusz.las@outlook.com>
2021-10-13 13:39:14 +0100

    removed merge conflict markers and fixed conditional rendering issue

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -10,59 +10,32 @@ function Dashboard() {
     const [routes, setRoutes] = useState();
     const [projects, setProjects] = useState();
     const [trees, setTrees] = useState();
+    const [error, setError] = useState();
 
     useEffect(() => getDashboard(), []);
 
     async function getDashboard() {
-        try {
+        try{
             const email = localStorage.getItem('email');
             const token = localStorage.getItem('token');
 
             const { data } = await axios.get(
                 `${process.env.REACT_APP_API_URL}/dashboard/${email}`,
-<<<<<<< HEAD
-                // { headers: { "Authorization": `Token ${token}`} }
-                { headers: { 'Authorization': `Token ${token}` } }
-=======
                 { headers: { "Authorization": `Token ${token}` } }
->>>>>>> 66e04709a8ccbee09aa8b5ddeb21212e4b9c8c08
             );
 
             console.log(data);
-
+            setProjects(data.projects);
             setUsername(data.first_name);
             setRoutes(data.routes);
             setTrees(data.n_trees);
-            setProjects(data.projects);
         } catch (err) {
             console.log(err);
-            // TO DO: add error handling
+            setError('Coud not fetch data');
         }
     }
 
-<<<<<<< HEAD
-    // dummy emissions data
-    const emissionsData = [
-        { id: 1, name: 'route 1', emissions_CO2e: 56, emissions_CO2e_km: 23, distance_km: 45 },
-        { id: 2, name: 'route 2', emissions_CO2e: 123, emissions_CO2e_km: 12, distance_km: 12 },
-        { id: 3, name: 'route 3', emissions_CO2e: 200, emissions_CO2e_km: 44, distance_km: 78 },
-        { id: 4, name: 'route 4', emissions_CO2e: 98, emissions_CO2e_km: 6, distance_km: 49 },
-        { id: 5, name: 'route 5', emissions_CO2e: 46, emissions_CO2e_km: 78, distance_km: 12 },
-        { id: 6, name: 'route 6', emissions_CO2e: 23, emissions_CO2e_km: 45, distance_km: 9 },
-        { id: 7, name: 'route 7', emissions_CO2e: 178, emissions_CO2e_km: 34, distance_km: 23 }
-    ]
-
-    // dummy projects
-    const projectsData = [
-        { project_title: 'Project 1', project_description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
-        { project_title: 'Project 2', project_description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
-        { project_title: 'Project 3', project_description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." }
-    ]
-
-    function renderProjects(projectsData) {
-=======
-    function renderProjects(projects) {
->>>>>>> 66e04709a8ccbee09aa8b5ddeb21212e4b9c8c08
+    const renderProjects = () => {
         // render list of project names with short description
 
         return projects.map((obj, i) =>
@@ -92,17 +65,13 @@ function Dashboard() {
                         <div>
                             <p className="h3">Active offsets</p>
                             <ul className="list-group">
-                                {renderProjects(projects)}
+                                { projects && renderProjects() }
                             </ul>
                         </div>
                     </div>
 
                     <div className="col-lg">
-<<<<<<< HEAD
-                        <RoutesList routes={emissionsData} />
-=======
-                        <RoutesList routes={routes}/>
->>>>>>> 66e04709a8ccbee09aa8b5ddeb21212e4b9c8c08
+                        { routes && <RoutesList routes={routes}/> }
                     </div>
 
                 </div>
