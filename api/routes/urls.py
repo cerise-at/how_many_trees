6189b9afbe5926321231ed3c6e8f48a63b51979d
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-10-13 11:56:47 +0100

    git add .

--- api/routes/urls.py
+++ api/routes/urls.py
@@ -2,9 +2,10 @@ from django.urls import include, path
 from . import views
 from django.conf.urls import url
 
-from .views import Directions, route_detail
+from .views import Directions, route_detail, create_route
 
 urlpatterns = [
     url(r'^id/(?P<route_id>.*)', route_detail, name='route_detail'),
+    url('create', create_route, name='create_route'),
     path('directions/', Directions.as_view(), name='directions'),
 ]
