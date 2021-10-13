55b2f07cc2350023602a7205d2c079827aea1e25
mariuszlas <mariusz.las@outlook.com>
2021-10-13 09:30:20 +0100

    added form submission handling

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -4,12 +4,11 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import './style.css';
 import axios from 'axios';
 
-function Map() {
+function Map({ selectedRoute }) {
 
     const mapContainer = useRef(null);
-    // const map = useRef(null);
     const [lng, setLng] = useState(-3.0);
-    const [lat, setLat] = useState(54.0);
+    const [lat, setLat] = useState(54.5);
     const [zoom, setZoom] = useState(4.1);
     const [routes, setRoutes] = useState();
 
@@ -17,15 +16,16 @@ function Map() {
 
     useEffect(async () => {
         async function getRoutes() {
-        try {
-            const { data } = await axios.get('https://api.mapbox.com/directions/v5/mapbox/driving/-0.135565%2C51.497452%3B-1.553621125%2C53.806948625000004?alternatives=true&geometries=geojson&steps=true&access_token=pk.eyJ1Ijoiam9uMjM1N3Nub3ciLCJhIjoiY2t1bG0wNnZlMWlvajJxbjZwcHAwNmFrdiJ9.GVXEHdzU73nmvJUXP49_DQ')
-            setRoutes(data.routes);
-            console.log('routes inside', data.routes);
-            return data.routes;
-        } catch (err) {
-            console.log(err);
+            try {
+                const { data } = await axios.get('https://api.mapbox.com/directions/v5/mapbox/driving/-0.135565%2C51.497452%3B-1.553621125%2C53.806948625000004?alternatives=true&geometries=geojson&steps=true&access_token=pk.eyJ1Ijoiam9uMjM1N3Nub3ciLCJhIjoiY2t1bG0wNnZlMWlvajJxbjZwcHAwNmFrdiJ9.GVXEHdzU73nmvJUXP49_DQ')
+                setRoutes(data.routes);
+                console.log('routes inside', data.routes);
+                return data.routes;
+            } catch (err) {
+                console.log(err);
+            }
         }
-    }
+
         const routesData = await getRoutes();
 
         const map = new mapboxgl.Map({
@@ -34,12 +34,11 @@ function Map() {
             container: mapContainer.current,
             style: 'mapbox://styles/mapbox/streets-v11',
             center: [lng, lat],
-            zoom: zoom
+            zoom: zoom,
+            maxZoom: 7
         });
 
         map.addControl(new mapboxgl.NavigationControl());
-
-        console.log('routes outside', routesData[0].geometry);
 
         map.once("load", function () {
            map.addSource('route', {
--- client/src/components/NewRouteForm/index.js
+++ client/src/components/NewRouteForm/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './style.css';
 
-function NewRouteForm({ setRoutes, setError }) {
+function NewRouteForm({ getDirections, error, setError }) {
 
     const [address1, setAddress1] = useState('');
     const [city1, setCity1] = useState('');
@@ -9,138 +10,170 @@ function NewRouteForm({ setRoutes, setError }) {
     const [address2, setAddress2] = useState('');
     const [city2, setCity2] = useState('');
     const [postcode2, setPostcode2] = useState('');
+    const [routeName, setRouteName] = useState('');
     const [vClass, setVClass] = useState();
     const [regNum, setRegNum] = useState('');
     const [isReoccuring, setIsReoccuring] = useState(false);
     const [date, setDate] = useState();
     const [freq, setFreq] = useState();
 
-    const renderOptions = () =>
+    const renderVehicleOptions = () =>
         ['van', 'lorry', 'pickup'].map((type, i) =>
         <option key={i} value={type}>{type}</option>)
 
-    const renderDates = () =>
+    const renderFreqOptions = () =>
         ['day', 'other day', 'week', 'two weeks', 'month'].map((date, i) =>
         <option key={i} value={date}>{date}</option>)
 
-    async function getDirections(e) {
+    function handleSubmit(e) {
         e.preventDefault();
 
-        try {
-            const email = localStorage.getItem('email');
-            const token = localStorage.getItem('token');
+        const email = localStorage.getItem('email');
+        const token = localStorage.getItem('token');
+        let vehicleData;
 
-            const url = `${process.env.REACT_APP_API_URL}/directions/?email=${email}&address1=${address1},${city1},${postcode1}&address2=${address2},${city2},${postcode2}&vehicle_class=${vClass}&registration_no=${regNum}`;
-            console.log(url);
-
-            //const { data } = axios.get(url, { headers: { "Authorization": token }});
-            // setRoutes(data.routes);
-        } catch (err) {
-            console.log(err);
-            setError(true);
+        // check if user provided only the registration number or the vehicle class
+        if ((!vClass && !regNum) || (vClass && regNum)) {
+            setError({ message: 'You must provide either the registration number or vehicle class' });
+            return;
+        } else if (vClass){
+            vehicleData = `vehicle_class=${vClass}`;
+        } else if (regNum) {
+            vehicleData = `registration_no=${regNum}`;
         }
-    }
 
-    function handle(e) {
-        e.preventDefault();
-        console.log(e.target);
+        const url = `${process.env.REACT_APP_API_URL}/directions/?email=${email}&route_name=${routeName}&address1=${address1},${city1},${postcode1}&address2=${address2},${city2},${postcode2}&${vehicleData}`;
+        getDirections(e, url)
     }
 
     return (
-        <form className="container" onSubmit={e => getDirections(e)}>
-            <div className="address">
-                <p className="h5">From:</p>
+        <>
+        <h1>Create a New Route</h1>
+        <main className="d-flex justify-content-center">
+            <form className="container row" id="new-route" onSubmit={e => handleSubmit(e)}>
+                <div className="address col-lg-6">
 
-                <input type="text"
-                        className="form-control col-md-12"
-                        onChange={e => setAddress1(e.target.value)}
-                        placeholder="Address" />
-
-                <div className="row">
-                    <input type="text"
-                            className="form-control col-md"
-                            onChange={e => setCity1(e.target.value)}
-                            placeholder="City"
-                            required />
+                    <p className="h4">Address</p>
+                    <p className="h5">From:</p>
 
                     <input type="text"
-                            className="form-control col-md"
-                            onChange={e => setPostcode1(e.target.value)}
-                            placeholder="Post Code"
-                            required />
-                </div>
-            </div>
-
-            <div className="address">
-                <p className="h5">To:</p>
-
-                <input type="text"
-                        className="form-control col-md-12"
-                        onChange={e => setAddress2(e.target.value)}
-                        placeholder="Address"
-                        required />
-
-                <div className="row">
-                    <input type="text"
-                            className="form-control col-md"
-                            onChange={e => setCity2(e.target.value)}
-                            placeholder="City"
-                            required />
-
-                    <input type="text"
-                            className="form-control col-md"
-                            onChange={e => setPostcode2(e.target.value)}
-                            placeholder="Post Code"
-                            required />
-                </div>
-            </div>
-
-            <div>
-                <p className="h5">Vehicle</p>
-
-                <input type="text"
-                        className="form-control"
-                        placeholder="Registration Number"
-                        onChange={e => setRegNum(e.target.value)}
-                        required />
-
-                <select defaultValue=""
-                        className="form-select"
-                        onChange={e => setVClass(e.target.value)}>
-
-                    <option value="" disabled hidden>Vehicle Class</option>
-                    { renderOptions() }
-                </select>
-            </div>
-
-            <div className="form-check">
-                <input className="form-check-input"
-                        type="checkbox"
-                        value=""
-                        id="datesCheckbox"
-                        onChange={e => setIsReoccuring(e.target.checked)} />
-
-                <label className="form-check-label" htmlFor="datesCheckbox">Multiple dates</label>
-            </div>
-
-            { isReoccuring &&
-                <div>
-                    <label htmlFor="datePicker">Choose Date:</label>
-                    <input type="date"
                             className="form-control"
-                            onChange={e => setDate(e.target.value)}/>
+                            onChange={e => setAddress1(e.target.value)}
+                            placeholder="Street" />
 
-                    <label htmlFor="freqSelector">Re-ocurring every:</label>
-                    <select defaultValue="" className="form-select" onChange={e => setFreq(e.target.value)}>
-                        { renderDates() }
-                    </select>
+                    <div className="container p-0">
+                        <div className="row">
+
+                            <div className="col-sm-8">
+                                <input type="text"
+                                        className="form-control"
+                                        onChange={e => setCity1(e.target.value)}
+                                        placeholder="City"
+                                        required />
+                            </div>
+
+                            <div className="col-sm-4">
+                                <input type="text"
+                                        className="form-control"
+                                        onChange={e => setPostcode1(e.target.value)}
+                                        placeholder="Post Code"
+                                        required />
+                            </div>
+                        </div>
+                    </div>
+
+                    <p className="h5">To:</p>
+
+                    <input type="text"
+                            className="form-control"
+                            onChange={e => setAddress2(e.target.value)}
+                            placeholder="Street"
+                            required />
+
+                    <div className="container p-0">
+                        <div className="row">
+
+                            <div className="col-sm-8">
+                                <input type="text"
+                                        className="form-control"
+                                        onChange={e => setCity2(e.target.value)}
+                                        placeholder="City"
+                                        required />
+                            </div>
+
+                            <div className="col-sm-4">
+                                <input type="text"
+                                        className="form-control"
+                                        onChange={e => setPostcode2(e.target.value)}
+                                        placeholder="Post Code"
+                                        required />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            }
 
-            <input type="submit"
-                    className="btn btn-outline-primary"
-                    value="Create New Route" />
-        </form>
+                <div className="col-lg-6">
+                    <div>
+                        <p className="h4">Route Name</p>
+                            <input type="text"
+                                    className="form-control"
+                                    onChange={e => setRouteName(e.target.value)}
+                                    required />
+
+                        <p className="h4">Vehicle</p>
+                        <p>If you know the vehicle registration number, enter it below. Otherwise, select a generic vehicle class.</p>
+
+                        <input type="text"
+                                className="form-control"
+                                onChange={e => setRegNum(e.target.value)}
+                                placeholder="Registration Number" />
+
+                        <select defaultValue=""
+                                className="form-select"
+                                onChange={e => setVClass(e.target.value)}>
+
+                            <option value="" disabled hidden>Vehicle Class</option>
+                            { renderVehicleOptions() }
+                        </select>
+                    </div>
+
+                    <div className="form-check">
+                        <input className="form-check-input"
+                                type="checkbox" value=""
+                                id="datesCheckbox"
+                                onChange={e => setIsReoccuring(e.target.checked)} />
+
+                        <label className="form-check-label" htmlFor="datesCheckbox">Multiple dates</label>
+                    </div>
+
+                    { isReoccuring &&
+                        <div>
+                            <label htmlFor="datePicker">Choose Date:</label>
+                            <input type="date"
+                                    className="form-control"
+                                    onChange={e => setDate(e.target.value)}/>
+
+                            <label htmlFor="freqSelector">Re-ocurring every:</label>
+                            <select defaultValue="" className="form-select"
+                                onChange={e => setFreq(e.target.value)}>
+                                { renderFreqOptions() }
+                            </select>
+                        </div>
+                    }
+                </div>
+
+                <div className="d-flex justify-content-center">
+                    <div className="d-flex flex-column">
+                        <input type="submit"
+                            className="btn btn-outline-primary"
+                            value="Create New Route" />
+
+                        { error && <p role="alert" className="alert alert-danger">{ error.message }</p> }
+                    </div>
+                </div>
+            </form>
+        </main>
+        </>
     )
 }
 
