933688542918feead434bf6418d8c631247d070e
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-10-13 12:11:05 +0100

    added missing comma

--- api/routes/urls.py
+++ api/routes/urls.py
@@ -7,6 +7,6 @@ from .views import Directions, route_detail, create_route, update_route
 urlpatterns = [
     url(r'^id/(?P<route_id>.*)', route_detail, name='route_detail'),
     url('create/', create_route, name='create_route'),
-    url('update/', update_route, name='update_route')
+    url('update/', update_route, name='update_route'),
     path('directions/', Directions.as_view(), name='directions'),
 ]
