51334f2af000cc88508aed899b514a121fb27a3b
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-10-13 11:57:04 +0100

    added create url

--- api/routes/urls.py
+++ api/routes/urls.py
@@ -6,6 +6,6 @@ from .views import Directions, route_detail, create_route
 
 urlpatterns = [
     url(r'^id/(?P<route_id>.*)', route_detail, name='route_detail'),
-    url('create', create_route, name='create_route'),
+    url('create/', create_route, name='create_route'),
     path('directions/', Directions.as_view(), name='directions'),
 ]
