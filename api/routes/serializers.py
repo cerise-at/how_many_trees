4abb6747f0a42cf978dbda670848edbccb4555a9
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-10-14 14:13:04 +0100

    added emissions to serializer

--- api/routes/serializers.py
+++ api/routes/serializers.py
@@ -5,6 +5,6 @@ class RouteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Route
         fields = [
-            'email', 'start_address', 'end_address', 'distance_km', 'name',
+            'email', 'start_address', 'end_address', 'distance_km', 'name', 'emissions',
             'vehicle_registration', 'vehicle_class', 'vehicle_emissions_CO2e_km', 'coords'
             ]
