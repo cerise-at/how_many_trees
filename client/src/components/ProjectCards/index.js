23761b4d03c0fb6f724bb0b2f13bce3f759888c8
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-10-14 14:01:55 +0100

    added conditional to renderCards

--- client/src/components/ProjectCards/index.js
+++ client/src/components/ProjectCards/index.js
@@ -5,47 +5,46 @@ import axios from 'axios';
 function ProjectCards() {
 
     const [projects, setProjects] = useState();
-    const [error, setError ] = useState();
+    const [error, setError] = useState();
     useEffect(() => getProjects(), []);
     async function getProjects() {
         try {
             const company = localStorage.getItem('company');
             const token = localStorage.getItem('token');
-            
+
             const { data } = await axios.get(
                 `${process.env.REACT_APP_API_URL}/projects/user/${company}`,
                 { headers: { "Authorization": `Token ${token}` } }
             );
             console.log(data)
             setProjects(data)
-            console.log(projects) 
+            console.log(projects)
 
         } catch (err) {
             console.log(err);
             setError('Coud not fetch data');
         }
     }
-        const renderCards = () =>
-        {
-            return projects.map((obj, i) => 
+    const renderCards = () => {
+        return projects && projects.map((obj, i) =>
             <div class="card" style="width: 18rem;">
                 {/* <img class="card-img-top" src="..." alt="Card image cap"> */}
-                    <div key={i} class="card-body">
-                        <h5 class="card-title">{obj.title}</h5>
-                            <h6 class="card-subtitle mb-2 text-muted">{obj.start_date}{obj.end_date}</h6>
-                            <p class="card-text">{obj.description}</p>
-                            <p class="card-text">{obj.offset_emissions_CO2e}</p>
-                                
-                     </div>
-            </div> )
-        }
+                <div key={i} class="card-body">
+                    <h5 class="card-title">{obj.title}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">{obj.start_date}{obj.end_date}</h6>
+                    <p class="card-text">{obj.description}</p>
+                    <p class="card-text">{obj.offset_emissions_CO2e}</p>
 
-        return (<>
-        <div class="card-group">{renderCards()}</div>
-
-        </>)
+                </div>
+            </div>)
     }
 
-    
+    return (<>
+        <div class="card-group">{renderCards()}</div>
+
+    </>)
+}
+
+
 
 export default ProjectCards;
