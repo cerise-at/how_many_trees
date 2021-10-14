a858d3599db54357b299df3f35b985338f31c03e
Sallan464 <stallan464:googlemail.com>
2021-10-14 20:29:41 +0100

    box scroll

--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -10,86 +10,40 @@ function Landing() {
             <div id="hero-image">
                 <div className="card">
                     <div className="w-100 d-flex justify-content-center">
-                        <h1 className="display-1">How Many Trees?</h1>
+                        <h1 className="display-1">How Many Trees...</h1>
                     </div>
                     <div className="w-100 d-flex justify-content-center">
-                        <hr />
-                    </div>
-                    {/* <SimpleSlider /> */}
-                    {/* <div className="vw-100"> */}
-                    {/* <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
-                            <div className="carousel-indicators">
-                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
-                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
-                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="Slide 6"></button>
-                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6" aria-label="Slide 7"></button>
-                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="7" aria-label="Slide 8"></button>
-                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="8" aria-label="Slide 9"></button>
-                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="9" aria-label="Slide 10"></button>
-                            </div>
-                            <div className="carousel-inner">
-                                <div className="carousel-item active">
-                                <div className="carousel-caption">
-                                    <h5>To offset my shipping emissions?</h5>
-                                </div>
-                                </div>
-                                <div className="carousel-item">
-                                <div className="carousel-caption">
-                                    <h5>To make my business sustainable?</h5>
-                                </div>
-                                </div>
-                                <div className="carousel-item">
-                                <div className="carousel-caption">
-                                    <h5>To remove one tonne of co2 from our air a year?</h5>
-                                </div>
-                                </div>
-                                <div className="carousel-item">
-                                <div className="carousel-caption">
-                                    <h5>To make my breakfast carbon neutral?</h5>
-                                </div>
-                                </div>
-                                <div className="carousel-item">
-                                <div className="carousel-caption">
-                                    <h5>To accountably deliver to my customers?</h5>
-                                </div>
-                                </div>
-                                <div className="carousel-item">
-                                <div className="carousel-caption">
-                                    <h5>To take care of the future?</h5>
-                                </div>
-                                </div>
-                                <div className="carousel-item">
-                                <div className="carousel-caption">
-                                    <h5>Should I plant to save the planet?</h5>
-                                </div>
-                                </div>
-                                <div className="carousel-item">
-                                <div className="carousel-caption">
-                                    <h5>Does it take to change a lightbulb?</h5>
-                                </div>
-                                </div>
-                                <div className="carousel-item">
-                                <div className="carousel-caption">
-                                    <h5>Crossed the road?</h5>
-                                </div>
-                                </div>
-                                <div className="carousel-item">
-                                <div className="carousel-caption">
-                                    <h5>Could a woodchuck if a woodchuck could chuck trees?</h5>
-                                </div>
+                        {/* <hr /> */}
+                        <div className="content-slider">
+                            <div className="slider">
+                                <div className="mask">
+                                <ul>
+                                    <li className="anim1">
+                                    <div className="quote">...to offset my shipping?</div>
+                                    </li>
+                                    <li className="anim2">
+                                    <div className="quote">...to accountably deliver to my customers ?</div>
+                                    </li>
+                                    <li className="anim3">
+                                    <div className="quote">...to make my business more sustainable?</div>
+                                    </li>
+                                    <li className="anim4">
+                                    <div className="quote">...does it take to draw down a ton of CO2?</div>
+                                    </li>
+                                    <li className="anim5">
+                                    <div className="quote">...to save the planet?</div>
+                                    </li>
+                                </ul>
                                 </div>
                             </div>
-                            </div> */}
-                    {/* </div> */}
+                            </div>
                     <section id="auth-buttons" className="w-100 d-flex justify-content-center">
                         <Link to="/auth/login" role="button" className="btn">Login</Link>
                         <Link to="/auth/register" role="button" className="btn">Sign Up</Link>
                     </section>
                 </div>
             </div>
+        </div>
         </main>
     );
 }
