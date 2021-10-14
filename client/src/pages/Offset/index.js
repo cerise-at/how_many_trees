0c2d23ad4d80763cbda91e471b6f98e68a4144f0
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-10-14 17:44:28 +0100

    added temporary favicon

--- client/src/pages/Offset/index.js
+++ client/src/pages/Offset/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { NavBar } from '../../layout';
 
 function Offset() {
@@ -8,6 +9,16 @@ function Offset() {
             <NavBar />
         </>
     );
+}
+
+
+const comingSoon = () => {
+    return (
+        <>
+            <h1>Coming Soon!</h1>
+            <NavLink to='dashboard'>Back to Home</NavLink>
+        </>
+    )
 }
 
 export default Offset;
